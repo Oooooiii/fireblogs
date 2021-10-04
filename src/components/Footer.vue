02ef903f84a68d7db74173a99f0c3a066f3587bf
Oooooiii <hearong.200@gmail.com>
2021-10-04 17:46:23 +0700

    Added Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="h-24 max-w-7xl mx-auto px-4">
+      <div class="h-full flex flex-col justify-center text-center border-t border-gray-300">
+        <p><strong>iBrong Community</strong> – A constructive and inclusive platform for software developers.</p> 
+        <p>With you every step of your journey.</p>
+      </div>
+  </footer>
+</template>
